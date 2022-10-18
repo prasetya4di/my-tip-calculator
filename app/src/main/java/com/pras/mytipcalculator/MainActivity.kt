cc8ae25699c47cb88cc18c554bd6d02162b89106
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-18 22:36:23 +0700

    Make edit number field editable

--- app/src/main/java/com/pras/mytipcalculator/MainActivity.kt
+++ app/src/main/java/com/pras/mytipcalculator/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -49,7 +49,8 @@ fun TipTimeScreen() {
 
 @Composable
 fun EditNumberField() {
-    TextField(value = "", onValueChange = {})
+    var amountInput by remember { mutableStateOf("0") }
+    TextField(value = amountInput, onValueChange = { amountInput = it })
 }
 
 @Preview(showBackground = true)
