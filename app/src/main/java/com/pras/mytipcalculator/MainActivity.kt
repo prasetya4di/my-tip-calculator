12158898b6da631a2754aed2ecd8d7925bb83070
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-18 22:40:41 +0700

    Update view for edit number field

--- app/src/main/java/com/pras/mytipcalculator/MainActivity.kt
+++ app/src/main/java/com/pras/mytipcalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,7 +52,14 @@ fun TipTimeScreen() {
 @Composable
 fun EditNumberField() {
     var amountInput by remember { mutableStateOf("0") }
-    TextField(value = amountInput, onValueChange = { amountInput = it })
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        label = { Text(stringResource(id = R.string.cost_of_service)) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 @Preview(showBackground = true)
