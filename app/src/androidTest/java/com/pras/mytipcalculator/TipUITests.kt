b6791a2f7f518165a355e92d1018e3e68601755f
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2022-10-18 23:05:13 +0700

    Add instrument test

---
+++ app/src/androidTest/java/com/pras/mytipcalculator/TipUITests.kt
@@ -0,0 +1,32 @@
+package com.pras.mytipcalculator
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.pras.mytipcalculator.ui.theme.MyTipCalculatorTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            MyTipCalculatorTheme {
+                TipTimeScreen()
+            }
+        }
+        composeTestRule
+            .onNodeWithText("Cost of Service")
+            .performTextInput("10")
+//        composeTestRule
+//            .onNodeWithText("Tip (%)")
+//            .performTextInput("20")
+        composeTestRule
+            .onNodeWithText("Tip Amount: $2.00")
+            .assertExists()
+    }
+}
